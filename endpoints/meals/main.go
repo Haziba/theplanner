6b6f3432e2ac1683cb4a9c3e0ae93cde8ca289ef
Haziba <harry.boyes@gmail.com>
2018-04-05 20:04:41 +0100

    Implement POST /meals

--- endpoints/meals/main.go
+++ endpoints/meals/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	helpers "github.com/haziba/theplanner/helpers"
 	"github.com/haziba/theplanner/models"
+	"github.com/haziba/theplanner/services/meal"
 )
 
 func handleRequest(context context.Context,
@@ -21,6 +22,42 @@ func handleRequest(context context.Context,
 		return helpers.CreateInternalServerErrorResponse()
 	}
 
+	if request.HTTPMethod == "POST" {
+		return post(request, mealService)
+	}
+
+	return get(mealService)
+}
+
+func post(request events.APIGatewayProxyRequest, mealService meal.MealService) (events.APIGatewayProxyResponse, error) {
+	var m models.Meal
+
+	err := json.Unmarshal([]byte(request.Body), &m)
+
+	if err != nil {
+		log.Printf("error unmarshalling meal: %v\n", err)
+		return helpers.CreateBadRequestResponse()
+	}
+
+	m, err = mealService.CreateMeal(m)
+	if err != nil {
+		log.Printf("error creating meal: %v\n", err)
+		return helpers.CreateBadRequestResponse()
+	}
+
+	data, err := json.Marshal(m)
+	if err != nil {
+		log.Printf("error marshalling meal: %v\n", err)
+		return helpers.CreateInternalServerErrorResponse()
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(data),
+		StatusCode: 200,
+	}, nil
+}
+
+func get(mealService meal.MealService) (events.APIGatewayProxyResponse, error) {
 	meals, err := mealService.GetAllMeals()
 
 	if err != nil {
