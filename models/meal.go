473928bc987b1de6bde9843497b90464ebe12387
Haziba <harry.boyes@gmail.com>
2018-04-09 20:42:18 +0100

    Add ingredients & time to meals

--- models/meal.go
+++ models/meal.go
@@ -1,6 +1,15 @@
 package models
 
 type Meal struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
+	Id         string `json:"id"`
+	Name       string `json:"name"`
+	TimeToMake struct {
+		Time       int `json: "time"`
+		Confidence int `json: "confidence"`
+	} `json:"time-to-make"`
+	Ingredients []struct {
+		Id       string `json:"id"`
+		Quantity int    `json:"quantity"`
+		Optional bool   `json:"optional"`
+	} `json:"ingredients"`
 }
